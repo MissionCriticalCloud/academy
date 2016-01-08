4e84bb532b85e23d66a53b0d6c1251fe75fd0154
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2016-01-08 11:57:22 +0100

    Initial Maven project structure.

---
+++ cosmic-checktyle/src/main/java/com/schubergphilis/mcc/academy/checkstyle/App.java
@@ -0,0 +1,13 @@
+package com.schubergphilis.mcc.academy.checkstyle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
